ad846c406b81969aacf02ae2dd1a8a9db6aac566
pabsan-0 <pablosantana@outlook.es>
2023-08-24 14:14:37 +0200

    vscode configs setup

--- aliases/configs.aliases.sh
+++ aliases/configs.aliases.sh
@@ -4,23 +4,21 @@ alias vim='vim -u $__REPO_PATH/configs/vim/.vimrc'
 alias code='code --user-data-dir $__REPO_PATH/configs/vscode/.config/Code'
 
 
-
-
 # VSCode requires sharing the whole config dir
-function __mls_vscode_clean () {
-    # find $__REPO_PATH/configs/vscode/ -type l -delete
+function __mlp_vscode_clean () {
+    shopt -s extglob
+
     rm -r $__REPO_PATH/configs/vscode/.config/Code/*/!(.gitignore)
     rm -r $__REPO_PATH/configs/vscode/.config/Code/!(.gitignore)
+
+    shopt -u extglob
 }
 
-function __mls_vscode_setup () {
+function __mlp_vscode_setup () {
     shopt -s extglob
     
-    # rm -r $__REPO_PATH/configs/vscode/.config/Code/*/!(.gitignore)
-    # ln -s  /home/$USER/.config/Code/!(User)    ls $__REPO_PATH/configs/vscode/.config/Code
-    # ln -s  /home/$USER/.config/Code/User/*      $__REPO_PATH/configs/vscode/.config/Code/User
     ln -sf $__REPO_PATH/configs/vscode/*.json  $__REPO_PATH/configs/vscode/.config/Code/User
 
     shopt -u extglob
 }
-__mls_vscode_setup
+__mlp_vscode_setup
