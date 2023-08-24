e00d76530db96b33436c773d68f4e2491bae532d
pabsan-0 <pablosantana@outlook.es>
2023-08-24 14:13:10 +0200

    mlp library, maybe rethink

---
+++ lib/mlp.sh
@@ -0,0 +1,17 @@
+mlp_find_up () {
+    path=$(pwd)
+    while [[ "$path" != "" && ! -e "$path/$1" ]]; do
+        path=${path%/*}
+    done
+    echo "$path"
+}
+
+mlp_this_file_path () {
+    SOURCE=${BASH_SOURCE[0]}
+    while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+        SOURCE=$(readlink "$SOURCE")
+        [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    echo $( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+}
