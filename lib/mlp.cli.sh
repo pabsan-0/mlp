b1eddac6d5faff4491fb598290758e3924fc6d1d
pabsan-0 <pablosantana@outlook.es>
2023-08-30 09:25:03 +0200

    add placeholders

---
+++ lib/mlp.cli.sh
@@ -0,0 +1,24 @@
+mlp_cli_help () {
+    :
+}
+
+mlp_cli_aliases () {
+    :
+    # grep aliases folder
+    # check if those are defined
+}
+
+mlp_cli_macros () {
+    :
+    # grep all scripts
+    # check if those are defined with 'declare -f | grep' 
+}
+
+mlp_cli_env () {
+    :
+    # printenv
+    # open subshell
+    # source environment in a subshell
+    # print subshell environment
+    # compare
+}
