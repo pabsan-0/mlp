886d269992a560e6494268cf29ad107b7bf4e988
pabsan-0 <psantana@catec.aero>
2023-09-25 14:01:13 +0200

    remove vscode

--- configs/mlp.configs.sh
+++ configs/mlp.configs.sh
@@ -2,18 +2,4 @@
 source $__MLP_PATH/configs/bash/.bashrc_extra
 alias tmux='tmux -f $__MLP_PATH/configs/tmux/.tmux.conf'
 alias vim='vim -N -u $__MLP_PATH/configs/vim/.vimrc'
-alias code='code --user-data-dir $__MLP_PATH/configs/vscode/.config/Code'
 
-
-# VSCode requires sharing the whole config dir
-__mlp_vscode_clean () {
-    shopt -s extglob
-    rm -r $__MLP_PATH/configs/vscode/.config/Code/*/!(.gitignore)
-    rm -r $__MLP_PATH/configs/vscode/.config/Code/!(.gitignore)
-    shopt -u extglob
-}
- 
-__mlp_vscode_setup () {
-    ln -sf $__MLP_PATH/configs/vscode/*.json  $__MLP_PATH/configs/vscode/.config/Code/User 
-}
-__mlp_vscode_setup
